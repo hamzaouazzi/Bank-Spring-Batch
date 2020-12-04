afbe62dc8d28a161c93dc9752e76059859244d9a
HAMZA OUAZZI <ouazzihamza97@gmail.com>
2020-12-04 12:31:24 +0100

    added Item Reader

--- src/main/java/org/id/bankspringbatch/config/SpringBatchConfiguration.java
+++ src/main/java/org/id/bankspringbatch/config/SpringBatchConfiguration.java
@@ -10,9 +10,17 @@ import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
 
 @Configuration
 @EnableBatchProcessing
@@ -35,4 +43,26 @@ public class SpringBatchConfiguration {
                 .start(step)
                 .build();
     }
+    @Bean
+    public FlatFileItemReader<BankTransaction> getItemReader(@Value("${inputFile}") Resource resource){
+        FlatFileItemReader<BankTransaction> flatFileItemReader=new FlatFileItemReader<>();
+        flatFileItemReader.setName("CSV-READER");
+        flatFileItemReader.setLinesToSkip(1);
+        flatFileItemReader.setResource(resource);
+        flatFileItemReader.setLineMapper(lineMapper());
+        return flatFileItemReader;
+    }
+    @Bean
+    public LineMapper<BankTransaction> lineMapper(){
+        DefaultLineMapper<BankTransaction> lineMapper=new DefaultLineMapper<>();
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+        lineTokenizer.setDelimiter(",");
+        lineTokenizer.setNames("id","accountID","strDate","transactionType","amount");
+        lineMapper.setLineTokenizer(lineTokenizer);
+        BeanWrapperFieldSetMapper<BankTransaction> fieldSetMapper=new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(BankTransaction.class);
+        lineMapper.setFieldSetMapper(fieldSetMapper);
+        return lineMapper;
+    }
+    
 }
